b3d619a14e136bf6d1119f8628ea98765a222d95
suwhisper <suwhisper@163.com>
2019-11-19 00:28:57 +0800

    Create hypotenuse.py

---
+++ chapter_6/hypotenuse.py
@@ -0,0 +1,25 @@
+def hypotenuse(x, y):
+	"""
+	Function that returns the length of the hypotenuse of a right triangle given the lengths of the other two legs as arguments.
+	"""
+	return 0.0
+
+def hypotenuse(x, y):
+	x1 = x ** 2
+	y1 = y ** 2
+	return 0.0
+
+def hypotenuse(x, y):
+	x1 = x ** 2
+	y1 = y ** 2
+	z1 = x1 +y1
+	return 0.0
+
+def hypotenuse(x, y):
+	x1 = x ** 2
+	y1 = y ** 2
+	z1 = x1 + y1
+	z = z1 ** 0.5
+	return z
+
+print(hypotenuse(5,6))
